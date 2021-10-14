224122580fd940098e68201dd0f4962052e3508e
nicosistemas <nicotarqui@gmail.com>
2021-10-13 23:32:58 -0300

    primer actions to docker

--- docker_django/hola_docker/views.py
+++ docker_django/hola_docker/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hola(request):
     context = {
-        "hola": "Hola desde Django-docker"
+        "hola": "Hola desde Django-dockerddg"
     }
     return render(request, 'hola.html', context)
