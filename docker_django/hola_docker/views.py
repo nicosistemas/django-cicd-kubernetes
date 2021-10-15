42b563e01628631d276994b1b74c3689652057db
Nico Tarquini <nicotarqui@gmail.com>
2021-10-15 19:11:16 -0300

    modifiqué el view

--- docker_django/hola_docker/views.py
+++ docker_django/hola_docker/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hola(request):
     context = {
-        "hola": "Hola desde Kubernetes!"
+        "hola": "Hola desde Kubernetes pero deployado con github actions, una locura esto!"
     }
     return render(request, 'hola.html', context)
