5c4810230f35f111a9ae08a1e990653077b3e4de
Nico Tarquini <45042953+nicosistemas@users.noreply.github.com>
2024-05-02 15:19:26 +0000

    agrego nuevo tags 1.33.0 en issuel.sh

--- issue.sh
+++ issue.sh
@@ -1,1 +1,1 @@
-print('este es un archivo agregado despues del issue para probar esta mecánica - version 1.2.0')
+print('este es un archivo agregado despues del issue para probar esta mecánica - version 1.3.0')
